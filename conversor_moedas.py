268e680f76da36c0bd154668d8dc68c2b3596c19
Stella Soares <stella.soares.ita@ext.quintoandar.com.br>
2023-11-26 19:12:48 -0300

    adicionando conversao dinheirinho

--- conversor_moedas.py
+++ conversor_moedas.py
@@ -1,6 +1,8 @@
 dinheiro = float(input("Quantos reais você tem? "))
 dolar = 4.95
 euro = 5.26
+dinheirinho = 2.4
 
 real_dolar = dinheiro/dolar
 real_euro = dinheiro/euro
+real_dinheirinho = dinheiro/dinheiro
